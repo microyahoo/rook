8af3b1a0d4e2a6e393dcf00e7c39bbf3fc83c19a
Liang Zheng <zhengliang0901@gmail.com>
2023-02-27 15:22:24 +0800

    refactor user quota
    Signed-off-by: Liang Zheng <zhengliang0901@gmail.com>

--- pkg/operator/ceph/object/user/controller.go
+++ pkg/operator/ceph/object/user/controller.go
@@ -296,35 +296,38 @@ func (r *ReconcileObjectStoreUser) createOrUpdateCephUser(u *cephv1.CephObjectSt
 	}
 
 	// Update max bucket if necessary
-	logger.Tracef("user capabilities(id: %s, caps: %#v, user caps: %s, op mask: %s)",
+	logger.Infof("user capabilities(id: %s, caps: %#v, user caps: %s, op mask: %s)",
 		user.ID, user.Caps, user.UserCaps, user.OpMask)
 	if *user.MaxBuckets != *r.userConfig.MaxBuckets {
-		user, err = r.objContext.AdminOpsClient.ModifyUser(r.opManagerContext, *r.userConfig)
+		logger.Infof("Try to update user max-buckets: (%d -> %d)", *user.MaxBuckets, *r.userConfig.MaxBuckets)
+		user, err = r.objContext.AdminOpsClient.ModifyUser(r.opManagerContext,
+			admin.User{ID: u.Name, MaxBuckets: r.userConfig.MaxBuckets})
 		if err != nil {
 			return errors.Wrapf(err, "failed to update ceph object user %q max buckets", r.userConfig.ID)
 		}
-		logCreateOrUpdate = fmt.Sprintf("updated ceph object user %q", u.Name)
+		logCreateOrUpdate = fmt.Sprintf("updated ceph object user max-buckets %q", u.Name)
 	}
 
+	logger.Infof("***r.userConfig: %+v", r.userConfig)
 	// Update caps if necessary
 	user.UserCaps = generateUserCaps(user)
 	if user.UserCaps != r.userConfig.UserCaps {
 		// If they are no caps to be removed, the API will return an error "missing user capabilities"
 		if user.UserCaps != "" {
-			logger.Tracef("remove capabilities %s from user %s", user.UserCaps, r.userConfig.ID)
+			logger.Infof("remove capabilities %s from user %s", user.UserCaps, r.userConfig.ID)
 			_, err = r.objContext.AdminOpsClient.RemoveUserCap(r.opManagerContext, r.userConfig.ID, user.UserCaps)
 			if err != nil {
 				return errors.Wrapf(err, "failed to remove current ceph object user %q capabilities", r.userConfig.ID)
 			}
 		}
 		if r.userConfig.UserCaps != "" {
-			logger.Tracef("set capabilities %s for user %s", r.userConfig.UserCaps, r.userConfig.ID)
+			logger.Infof("set capabilities %s for user %s", r.userConfig.UserCaps, r.userConfig.ID)
 			_, err = r.objContext.AdminOpsClient.AddUserCap(r.opManagerContext, r.userConfig.ID, r.userConfig.UserCaps)
 			if err != nil {
 				return errors.Wrapf(err, "failed to update ceph object user %q capabilities", r.userConfig.ID)
 			}
 		}
-		logCreateOrUpdate = fmt.Sprintf("updated ceph object user %q", u.Name)
+		logCreateOrUpdate = fmt.Sprintf("updated ceph object user caps %q", u.Name)
 	}
 
 	var quotaEnabled = false
@@ -340,15 +343,27 @@ func (r *ReconcileObjectStoreUser) createOrUpdateCephUser(u *cephv1.CephObjectSt
 			quotaEnabled = true
 		}
 	}
-	userQuota := admin.QuotaSpec{
-		UID:        u.Name,
-		Enabled:    &quotaEnabled,
-		MaxSize:    &maxSize,
-		MaxObjects: &maxObjects,
+	// enable or disable quota for user
+	if *user.UserQuota.Enabled != quotaEnabled {
+		err = r.objContext.AdminOpsClient.SetUserQuota(r.opManagerContext, admin.QuotaSpec{UID: u.Name, Enabled: &quotaEnabled})
+		if err != nil {
+			return errors.Wrapf(err, "failed to set user %q quota enabled=%v", u.Name, quotaEnabled)
+		}
+		logCreateOrUpdate = fmt.Sprintf("updated ceph object user quota %q to %t", u.Name, quotaEnabled)
 	}
-	err = r.objContext.AdminOpsClient.SetUserQuota(r.opManagerContext, userQuota)
-	if err != nil {
-		return errors.Wrapf(err, "failed to set quotas for user %q", u.Name)
+
+	if quotaEnabled {
+		userQuota := admin.QuotaSpec{
+			UID:        u.Name,
+			MaxSize:    &maxSize,
+			MaxObjects: &maxObjects,
+		}
+		logger.Infof("**set user quota(%d, %d) for user %s", *userQuota.MaxSize, *userQuota.MaxObjects, u.Name)
+		err = r.objContext.AdminOpsClient.SetUserQuota(r.opManagerContext, userQuota)
+		if err != nil {
+			return errors.Wrapf(err, "failed to set quotas for user %q", u.Name)
+		}
+		logCreateOrUpdate = fmt.Sprintf("updated ceph object user quota %q", u.Name)
 	}
 
 	// Set access and secret key
